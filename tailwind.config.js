c3c34da662634bb69e692cfb791c6e5889cc19c5
rdsgabriel <gabriel.rodrigues.ismart@gmail.com>
2024-07-31 16:16:15 -0300

    first

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: { colors:{
+      azul1: '#4F3FFF',
+      azul2: '#5D4FFB',
+      azul3: '#756AF5',
+      branco: '#F3F3F5',
+      preto: '#2C2C2C'
+    },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+};
